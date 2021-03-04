2e9fa15c462c0f153be134e154821b687ce034a4
David Satime Wallin <david.wallin@klaravik.se>
2021-03-04 15:12:21 +0100

    minimalistic colorscheme
    Signed-off-by: David Satime Wallin <david.wallin@klaravik.se>

---
+++ duzcol.lua
@@ -0,0 +1,6 @@
+VERSION = "1.0.0"
+
+local config = import("micro/config")
+
+config.AddRuntimeFile("duzcol", config.RTColorscheme, "colorschemes/duzcol.micro")
+config.AddRuntimeFile("duzcol", config.RTHelp, "help/duzcol.md")
